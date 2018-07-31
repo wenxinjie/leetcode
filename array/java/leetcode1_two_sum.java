42c4150f4598c2b94af8cd4441c9d5d70554de55
Xinjie Wen <wenxinjieinnyc@gmail.com>
2018-07-31 16:04:33 -0400

    20180731 array java 1

---
+++ array/java/leetcode1_two_sum.java
@@ -0,0 +1,21 @@
+package wenxinjie;
+
+import java.util.HashMap;
+
+public class leetcode1_two_sum {
+	public int[] twoSum(int[] nums, int target) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int[] defaultres = { 0, 0 };
+		for (int i = 0; i < nums.length; i++) {
+			if (map.get(target - nums[i]) != null) {
+				int[] res = {i, map.get(target - nums[i])};
+				return res;
+			}
+			map.put(nums[i], i);
+		}
+		return defaultres;
+	}
+}
+
+//time:O(n)
+//space: O(n)
