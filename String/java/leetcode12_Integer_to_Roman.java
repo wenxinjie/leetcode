fcad080af11ff731faadd76dd896200dbfccb200
Xinjie Wen <wenxinjieinnyc@gmail.com>
2018-07-31 21:37:01 -0400

    string

---
+++ String/java/leetcode12_Integer_to_Roman.java
@@ -0,0 +1,40 @@
+//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+//
+//Symbol       Value
+//I             1
+//V             5
+//X             10
+//L             50
+//C             100
+//D             500
+//M             1000
+//For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+//
+//Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+//
+//I can be placed before V (5) and X (10) to make 4 and 9. 
+//X can be placed before L (50) and C (100) to make 40 and 90. 
+//C can be placed before D (500) and M (1000) to make 400 and 900.
+//Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
+
+package wenxinjie;
+
+public class leetcode12_Integer_to_Roman {
+	public String intToRoman(int num) {
+        String s = "";
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] strs = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        for (int i = 0; i < values.length; i ++){
+            while (num >= values[i]){
+                s += strs[i];
+                num -= values[i];
+            }
+        }
+        return s;
+    }
+
+}
+
+//Time: O(1)
+//Space: O(1)
+//Difficulty: medium
