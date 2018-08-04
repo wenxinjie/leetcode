db6f6865fd11d543fdb9e7e31f8207618611566f
Xinjie Wen <wenxinjieinnyc@gmail.com>
2018-08-04 15:20:12 -0400

    modify

---
+++ dynamic programming/python/leetcode63_Unique_Paths_II.py
@@ -0,0 +1,27 @@
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+# The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+# Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [0 for _ in range(n)]
+        dp[0] = 1
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j-1]
+        return dp[n-1]
+                    
+# Time: O(m*n)
+# Space: O(n)
+# Difficulty: medium
