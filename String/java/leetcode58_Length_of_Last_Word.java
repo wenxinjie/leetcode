c3719f795850f342a2085163be94eb82a229179d
wenxinjie <38590187+wenxinjie@users.noreply.github.com>
2018-08-04 22:10:33 -0400

    modify

--- String/java/leetcode58_Length_of_Last_Word.java
+++ String/java/leetcode58_Length_of_Last_Word.java
@@ -1,5 +1,8 @@
 package wenxinjie;
 
 public class leetcode58_Length_of_Last_Word {
-
+  public int lengthOfLastWord(String s) {
+        int n = s.trim().lastIndexOf(" ") + 1;
+        return s.trim().length() - n;    
+	}	
 }
